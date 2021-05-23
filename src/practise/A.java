35e217bef61b99765a9fab5fece668357d51a5ae
shivam <shivamgokarankar86@gmail.com>
2021-05-23 21:37:16 +0530

    deep cloning program

--- src/practise/A.java
+++ src/practise/A.java
@@ -1,13 +1,39 @@
 package practise;
 
-public class A {
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.atomic.AtomicLongArray;
 
-
-    public static void main( String args[] )
+class Cat
+{
+    int  j ;
+    Cat(int j)
     {
+        this.j=j;
+    }
+}
+class Dog implements Cloneable
+{
+    int i ;
+    Cat c1 ;
+    Dog(Cat c , int i )
+    {
+        this.c1 = c ;
+        this.i = i ;
 
-        Integer i = 10;
-        Float f= Float.valueOf(10.5f);
-        System.out.println(i +" " + f);
+    }
+    public Object Clone() throws CloneNotSupportedException {
+        Cat c =  new Cat(c1.j);
+        Dog d1= new Dog(c,i);
+        return d1;
+
+    }
+}
+public class A implements Cloneable {
+    public static void main(String[] args) throws CloneNotSupportedException
+    {
+        Cat c2 = new Cat(10);
+        Dog d1 = new Dog(c2,20);
+        Dog d2=  (Dog)d1.Clone();
+        System.out.println(d2.i + " " + d2.c1.j);
     }
 }
