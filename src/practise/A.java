180698bcf3766a3227f4f73bcfa1462417cdfae8
shivam <shivamgokarankar86@gmail.com>
2021-05-30 18:45:30 +0530

    main thread interupting child thread

--- src/practise/A.java
+++ src/practise/A.java
@@ -7,21 +7,18 @@ import java.util.concurrent.atomic.AtomicLongArray;
 
 class myThread extends  Thread
 {
-
-    static Thread mt ;
-
     public void run()
     {
         try
         {
-            mt.join();
+            for (int i = 0; i <10 ; i++) {
+                System.out.println("child ");
+                Thread.sleep(1000);
+            }
         }
         catch (InterruptedException e)
         {
-
-        }
-        for (int i = 0; i <10 ; i++) {
-            System.out.println("child thread ");
+            System.out.println("got interupted ");
         }
     }
 
@@ -30,14 +27,9 @@ class myThread extends  Thread
 public class A
 {
     public static void main(String[] args) throws InterruptedException {
-
-        myThread.mt=Thread.currentThread();
         myThread t = new myThread();
         t.start();
-        for (int i = 0; i < 10; i++) {
-            System.out.println("main thread ");
-            Thread.sleep(1000);
-        }
+        t.interrupt();
     }
 }
 
