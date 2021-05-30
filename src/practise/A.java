c3cf0d9c6525e1712d3b40f81f74a2894f08c898
shivam <shivamgokarankar86@gmail.com>
2021-05-30 18:20:26 +0530

    stoping child thread until main thread completes code

--- src/practise/A.java
+++ src/practise/A.java
@@ -3,37 +3,42 @@ package practise;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.atomic.AtomicLongArray;
 
-class Cat
-{
-    int  j ;
-    Cat(int j)
-    {
-        this.j=j;
-    }
-}
-class Dog implements Cloneable
-{
-    int i ;
-    Cat c1 ;
-    Dog(Cat c , int i )
-    {
-        this.c1 = c ;
-        this.i = i ;
 
-    }
-    public Object Clone() throws CloneNotSupportedException {
-        Cat c =  new Cat(c1.j);
-        Dog d1= new Dog(c,i);
-        return d1;
 
-    }
-}
-public class A implements Cloneable {
-    public static void main(String[] args) throws CloneNotSupportedException
+class myThread extends  Thread
+{
+
+    static Thread mt ;
+
+    public void run()
     {
-        Cat c2 = new Cat(10);
-        Dog d1 = new Dog(c2,20);
-        Dog d2=  (Dog)d1.Clone();
-        System.out.println(d2.i + " " + d2.c1.j);
+        try
+        {
+            mt.join();
+        }
+        catch (InterruptedException e)
+        {
+
+        }
+        for (int i = 0; i <10 ; i++) {
+            System.out.println("child thread ");
+        }
+    }
+
+}
+
+public class A
+{
+    public static void main(String[] args) throws InterruptedException {
+
+        myThread.mt=Thread.currentThread();
+        myThread t = new myThread();
+        t.start();
+        for (int i = 0; i < 10; i++) {
+            System.out.println("main thread ");
+            Thread.sleep(1000);
+        }
     }
 }
+
+
